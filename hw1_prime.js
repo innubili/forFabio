c9e3e96157043dc16d8b909faf4a14bd0354cd56
innubili <rudy@innubili.com>
2013-06-22 15:47:33 +0200

    updated with file system

--- hw1_prime.js
+++ hw1_prime.js
@@ -1,3 +1,14 @@
+var fs = require('fs');
+
+var outfile = "prime_numbers.txt";
+
+
+var primeNumbers = getPrimeNumbers(100);
+var content = primeNumbers.join(', ');
+fs.writeFileSync(outfile, content);  
+
+console.log("Script: " + __filename + "\nWrote some numbers To: " + outfile);
+
 function getPrimeNumbers(n){
   if (n < 2) return [];
   var result = [2];
@@ -10,14 +21,10 @@ function getPrimeNumbers(n){
 
     if (!notMultiple) {
       result.push(i);
+      console.log("Script: getPrimeNumbers added' + i + " to the list");
     }
   }
 
   return result;
 }
 
-function write100primeNumbersToFile(){
-  var primeNumbers = getPrimeNumbers(100);
-  var content = primeNumbers.join(', ');
-  fs = ...
-}
